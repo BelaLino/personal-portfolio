7360e83d74d6b6d5107b589b8ede36f781ba5fec
Andress <andressmartin.aka@gmail.com>
2023-12-13 04:01:16 -0300

    Fixed navBar and footer links

--- personal-portfolio/src/components/NavBar.js
+++ personal-portfolio/src/components/NavBar.js
@@ -50,12 +50,12 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+                {/* <a href="#"><img src={navIcon1} alt="" /></a> */}
+                {/* <a href="#"><img src={navIcon2} alt="" /></a>
+                <a href="#"><img src={navIcon3} alt="" /></a> */}
               </div>
               <HashLink to='#connect'>
-                <button className="vvd"><span>Let’s Connect</span></button>
+                <button className="vvd"><span>Fale Conosco</span></button>
               </HashLink>
             </span>
           </Navbar.Collapse>
